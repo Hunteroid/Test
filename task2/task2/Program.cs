1c47b3b4b27679eb61441cdf759a0f86d93277c5
Hunteroid <olegant@ukr.net>
2013-09-03 13:30:19 +0300

    final

--- task2/task2/Program.cs
+++ task2/task2/Program.cs
@@ -15,7 +15,8 @@ namespace task2
     class Triangle
     {
         //поля (длины сторон)
-        private int a, b, c; 
+        private int a, b, c;
+        public string color;
 
         //свойства (доступ к полям)
         public int A
@@ -81,11 +82,15 @@ namespace task2
         static void Main(string[] args)
         {
             Console.WriteLine("Программа для рассчета периметра и площади треугольника!");
+
+            Console.Write("Введите цвет треугольника: ");
+            string color = Console.ReadLine();
+            Console.WriteLine();
+            
             Console.Write("Введите длину стороны а: ");
             int a = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine();
-            Console.WriteLine();
-
+            Console.WriteLine();                   
+            
             Console.Write("Введите длину стороны b: ");
             int b = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
@@ -100,6 +105,7 @@ namespace task2
             
             Console.WriteLine("Периметр треугольника равен: {0}", instanse.Perimeter());
             Console.WriteLine("Площадь треугольника равна: {0}", instanse.Square());
+            Console.WriteLine("Цвет треугольника {0}", color);
 
             Console.ReadKey();            
 
